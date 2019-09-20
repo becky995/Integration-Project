effcfd8c5f91d8a48deddebba040441b29d8e24c
becky995 <54412519+becky995@users.noreply.github.com>
2019-09-20 17:12:52 -0400

    Add files via upload

---
+++ project1.py
@@ -0,0 +1,10 @@
+#Becky Perez
+#Sticker Pricing
+name = input("What is your name?")
+print("Hello," , name)
+len = input("What is the length? ")
+num1 = int(len)
+wid = input("What is the width? ")
+num2 = int(wid)
+lxw = num1 *num2
+print("The length times width is", lxw)
