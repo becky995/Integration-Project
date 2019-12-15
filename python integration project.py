9aa536fe81c61ee073ca969bb6c0f28da2019b32
becky995 <54412519+becky995@users.noreply.github.com>
2019-12-14 23:30:43 -0500

    Add files via upload
    Resubmisson

--- python integration project.py
+++ python integration project.py
@@ -7,7 +7,6 @@ name = input("What is your name?")
 print("Hello,", name)
 tos = int(input("Would you like a sticker or a t-shirt? (Enter 1 for "
             "sticker, 2 for t-shirt)"))
-# What if they want both
 input_is_bad = True
 while input_is_bad:
     try:
@@ -17,22 +16,31 @@ while input_is_bad:
         print("The amount wanted is:", quantity)
     except ValueError:
         print("This is not a whole number.")
-color = input("What color material would you like?")
+color = (input("What color material would you like?"))
 input_is_bad = True
 while input_is_bad:
     try:
-        length = int(input("What is your desire length for image (in inches)? "
-                           "Enter as an integer or decimal number."))
+        length = (input("What is your desire length for image (in inches)? "
+                           "Enter as an integer."))
+        input_is_bad = False
     except ValueError:
-        print("This is not an integer or rational number.")
+       print("This is not an integer.")
 input_is_bad = True
 while input_is_bad:
     try:
         width = int(input("What is your desire width for image (in inches)? "
-                          "Enter as an integer or rational number."))
+                          "Enter as an integer."))
         input_is_bad = False
         print("The width wanted is:", width)
     except ValueError:
-        print("This is not an integer or rational number.")
+        print("This is not an integer.")
 getarea = length * width
-print("The desire pruduct is:", getarea)
+print("The desire pruduct is:", quantity,color,tos, getarea)
+delivery = int(input("What delivery would you like: enter 1 for standard, "
+                     "2 for 3 day shipping, and 3 for 1 day shipping"))
+if delivery <= 1:
+    print("There is no additional fee.")
+elif delivery >= 2:
+    print("There is a $3.00 shipping fee.")
+else:
+    print("There is a $5.00 shipping fee.")
